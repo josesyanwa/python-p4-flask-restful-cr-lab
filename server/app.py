0279c013fdc7ef4ac0960a68a7be17b11506891b
Joses Syanwa <josesyanwa@gmail.com>
2024-01-26 09:16:38 +0300

    Completed

--- server/app.py
+++ server/app.py
@@ -1,15 +1,11 @@
-#!/usr/bin/env python3
-
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
-
+from flask_restful import Api, Resource, reqparse
 from models import db, Plant
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = True
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -17,11 +13,32 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return jsonify([plant.to_dict() for plant in plants])
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, plant_id):
+        plant = Plant.query.get_or_404(plant_id)
+        return jsonify(plant.to_dict())
+
+class CreatePlant(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('name', type=str, required=True, help='Name cannot be blank')
+        self.reqparse.add_argument('image', type=str, required=True, help='Image cannot be blank')
+        self.reqparse.add_argument('price', type=float, required=True, help='Price cannot be blank')
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        new_plant = Plant(**args)
+        db.session.add(new_plant)
+        db.session.commit()
+        return jsonify(new_plant.to_dict()), 201
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:plant_id>')
+api.add_resource(CreatePlant, '/plants/create')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
